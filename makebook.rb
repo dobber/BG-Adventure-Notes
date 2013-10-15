4b1c4f685f1d099540cb454c0170595830e77541
Ivan Dimitrov <zlobber@gmail.com>
2013-10-15 18:54:25 +0300

    Finaly made the propper markup language, first book now converts to html

--- makebook.rb
+++ makebook.rb
@@ -18,14 +18,17 @@ licence='Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported'
 
 book_content = %(<html xmlns="http://www.w3.org/1999/xhtml"><head><title>#{book_title}</title></head><body>)
 dir = File.expand_path(File.join(File.dirname(__FILE__), 'books'))
-Dir[File.join(dir, '**', '*.txt')].sort.each do |input|
-  puts "processing #{input}"
-  content = File.read(input)
-  content.gsub!(/Insert\s18333fig\d+\.jpg\s*\n.*?(\d{1,2})-(\d{1,2})\. (.*)/, '![\1.\2 \3](dir/Images/\1.\2.jpg "\1.\2 \3")')
-  book_content << RDiscount.new(content).to_html
+Dir[File.join(dir, '**')].sort.each do |booksdir|
+  Dir[File.join(booksdir, '*.txt')].sort.each do |input|
+    puts "processing #{input}"
+    content = File.read(input)
+    content.gsub!(/Insert\s(\d+)\.jpg/, '![\1](' + booksdir + '/Images/\1.jpg)')
+    book_content << RDiscount.new(content).to_html
+    end
 end
 
 book_content << "</body></html>"
+
 File.open("bg-adventure-notes.html", 'w') do |output|
   output.write(book_content)
 end
