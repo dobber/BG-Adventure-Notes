286ee66bdbfd78f8589e68e845559d8eaf55c2f8
Ivan Dimitrov <zlobber@gmail.com>
2013-10-16 00:39:15 +0300

    makebook.rb now works fine.

--- makebook.rb
+++ makebook.rb
@@ -5,17 +5,28 @@ require 'rdiscount'
 require 'fileutils'
 include FileUtils
 
+ebookconvert='/Applications/calibre.app/Contents/MacOS/ebook-convert'
+book_filename='bg-adventure-notes'
+
+book_title='Записки по българските приключения'
+authors='Сборник разкази'
+licence='Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported'
+language='bg'
+
 if ARGV.length == 0
-  puts "you need to specify book format (mobi, epub, html). For example: ./makebook epub"
+  puts "You need to specify book format (all, fb2, mobi, epub, html).\nFor example: ./makebook epub"
   exit
 end
 
 format=ARGV[0] 
 
-book_title='Записки по българските приключения'
-authors='Сборник разкази'
-licence='Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported'
+# define methods
+def to_book(cmd, format)
+  cmd.gsub!(/format/, "#{format}")
+  system (cmd)
+end
 
+#begin main()
 book_content = %(<html xmlns="http://www.w3.org/1999/xhtml"><head><title>#{book_title}</title></head><body>)
 dir = File.expand_path(File.join(File.dirname(__FILE__), 'books'))
 Dir[File.join(dir, '**')].sort.each do |booksdir|
@@ -29,6 +40,23 @@ end
 
 book_content << "</body></html>"
 
-File.open("bg-adventure-notes.html", 'w') do |output|
+File.open("#{book_filename}.html", 'w') do |output|
   output.write(book_content)
+end
+
+cmd="#{ebookconvert} #{book_filename}.html #{book_filename}.format --authors '#{authors}' --comments '#{licence}' --level1-toc //h:h1 --level2-toc //h:h2 --level3-toc //h:h3 --language #{language}"
+
+if format == 'all'
+  to_book(cmd,'mobi')
+  to_book(cmd,'epub')
+  to_book(cmd,'fb2')
+elsif format == 'mobi'
+  to_book(cmd,format)
+elsif format == 'epub'
+  to_book(cmd,format)
+elsif format == 'fb2'
+  to_book(cmd,format)
+elsif format =='html'
+else
+  exit 0
 end
